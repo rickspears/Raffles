f38f7d787480ea0f9047fa991096894f97dfeaa8
rickspears <valiryonh@yahoo.com>
2013-08-15 22:34:29 -0700

    Refactored for Unit Of Work
    ViewModelLocator:
    * Updated to use RepositoryProvider, RepositoryFactories, UnitOfWork
    * Updated to pass the UoW as an argument into RaffleWinnersViewModel
    * Updated so ViewModels no longer always return nulls.

--- Raffles.ViewModels/Locator/ViewModelLocator.cs
+++ Raffles.ViewModels/Locator/ViewModelLocator.cs
@@ -3,39 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Raffles.Data.Services.Factory;
+using Raffles.Data.Services.UnitOfWork;
 
 namespace Raffles.ViewModels.Locator
 {
     public class ViewModelLocator
     {
+        private static RepositoryFactories factory = new RepositoryFactories();
+        private static IRepositoryProvider provider = new RepositoryProvider(factory);
+        private static IUnitOfWork uow = new UnitOfWork(provider);
+
         private static MainPageViewModel mainPageViewModel;
         public static MainPageViewModel MainPageViewModel {
-            get { return mainPageViewModel != null ? mainPageViewModel : new MainPageViewModel(); }            
+            get {
+                if (mainPageViewModel == null)
+                    mainPageViewModel = new MainPageViewModel();
+                return mainPageViewModel;
+            }            
         }
 
         private static ManageItemsViewModel manageItemsViewModel;
         public static ManageItemsViewModel ManageItemsViewModel {
-            get { return manageItemsViewModel != null ? manageItemsViewModel : new ManageItemsViewModel(); }
+            get {
+                if (manageItemsViewModel == null)
+                    manageItemsViewModel = new ManageItemsViewModel();
+                return manageItemsViewModel;
+            }
         }
 
         private static ManageParticipantsViewModel manageParticipantsViewModel;
         public static ManageParticipantsViewModel ManageParticipantsViewModel {
-            get { return manageParticipantsViewModel != null ? manageParticipantsViewModel : new ManageParticipantsViewModel(); }
+            get {
+                if (manageParticipantsViewModel == null)
+                    manageParticipantsViewModel = new ManageParticipantsViewModel();
+                return manageParticipantsViewModel;
+            }
         }
 
         private static ManageRafflesViewModel manageRafflesViewModel;
         public static ManageRafflesViewModel ManageRafflesViewModel {
-            get { return manageRafflesViewModel != null ? manageRafflesViewModel : new ManageRafflesViewModel(); }
+            get {
+                if (manageRafflesViewModel == null)
+                    manageRafflesViewModel = new ManageRafflesViewModel();
+                return manageRafflesViewModel;
+            }
         }
 
         private static RafflePlayViewModel rafflePlayViewModel;
         public static RafflePlayViewModel RafflePlayViewModel {
-            get { return rafflePlayViewModel != null ? rafflePlayViewModel : new RafflePlayViewModel(); }
+            get {
+                if (rafflePlayViewModel == null)
+                    rafflePlayViewModel = new RafflePlayViewModel();
+                return rafflePlayViewModel;
+            }
         }
 
         private static RaffleWinnersViewModel raffleWinnersViewModel;
         public static RaffleWinnersViewModel RaffleWinnersViewModel {
-            get { return raffleWinnersViewModel != null ? raffleWinnersViewModel : new RaffleWinnersViewModel(); }
+            get {
+                if (raffleWinnersViewModel == null)
+                    raffleWinnersViewModel = new RaffleWinnersViewModel(uow);
+                return raffleWinnersViewModel;
+            }
         }
     }
 }
